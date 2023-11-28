e024bd880f9b42d1cb1c69b81a3472251a984b15
pixelvip <pixelmannvip@gmail.com>
2023-11-28 23:44:09 +0100

    Add gitlab nvim and diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,5 @@
 return {
+  -- git signs on the side, also git blame (custom key map)
   {
     "lewis6991/gitsigns.nvim",
     opts = {
@@ -25,5 +26,48 @@ return {
         -- stylua: ignore end
       end,
     },
+  },
+
+  -- gitlab nvim
+  {
+    "harrisoncramer/gitlab.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+      enabled = true,
+    },
+    build = function()
+      require("gitlab.server").build(true)
+    end, -- Builds the Go binary
+    config = function()
+      local gitlab = require("gitlab")
+      gitlab.setup({
+        reviewer = "diffview",
+      })
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      map("n", "<leader>glr", gitlab.review, "review")
+      map("n", "<leader>gls", gitlab.summary, "summary")
+      map("n", "<leader>glA", gitlab.approve, "approve")
+      map("n", "<leader>glR", gitlab.revoke, "revoke")
+      map("n", "<leader>glc", gitlab.create_comment, "create comment")
+      map("n", "<leader>gln", gitlab.create_note, "create note")
+      map("n", "<leader>gld", gitlab.toggle_discussions, "toggle discussions")
+      map("n", "<leader>glaa", gitlab.add_assignee, "add assignee")
+      map("n", "<leader>glad", gitlab.delete_assignee, "delete assignee")
+      -- map("n", "<leader>glra", gitlab.add_reviewer, "add reviewer")
+      -- map("n", "<leader>glrd", gitlab.delete_reviewer, "delete reviewer")
+      map("n", "<leader>glp", gitlab.pipeline, "pipeline")
+      map("n", "<leader>glo", gitlab.open_in_browser, "open in browser")
+    end,
+  },
+
+  -- git diffview
+  {
+    "sindrets/diffview.nvim",
   },
 }
