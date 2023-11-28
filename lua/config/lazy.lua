dc47c60e8d43c15fed7b11d16239f53f8f9ea08f
pixelvip <pixelmannvip@gmail.com>
2023-11-28 23:44:08 +0100

    Add phyton

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.python" },
 
     -- formatting
     { import = "lazyvim.plugins.extras.formatting.prettier" },
